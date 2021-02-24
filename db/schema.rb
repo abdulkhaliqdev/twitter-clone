b9747f8f9c1eee2a58089fe3a26c5a67ef680780
abdulkhaliqdev <abdulkhaliql1f18bscs0159@gmail.com>
2021-02-25 00:49:34 +0500

    Add user profile view and associate the user-id with tweets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_153005) do
+ActiveRecord::Schema.define(version: 2021_02_24_165109) do
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_02_24_153005) do
     t.text "tweet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
